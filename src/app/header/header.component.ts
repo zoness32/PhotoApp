e7f1531ace403a91a2e401a5a6902567fec549a7
Taylor <teernisse@gmail.com>
2018-03-10 13:44:57 -0800

    update endpoint

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -25,13 +25,13 @@ export class HeaderComponent implements OnInit {
   }
 
   getGalleries() {
-    return this.httpClient.get(`http://192.168.1.14:3000/dirs.json`)
+    return this.httpClient.get(`http://tayloreernisse.website:5000/dirs.json`)
       .subscribe(data => {
         this.galleries = data;
       });
   }
 
   getGalleryUri(galleryName: string) {
-    return `http://192.168.1.14:3000/galleries/${galleryName}/data.json`;
+    return `http://tayloreernisse.website:5000/galleries/${galleryName}/data.json`;
   }
 }
